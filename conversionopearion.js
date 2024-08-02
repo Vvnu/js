f304a26d7908b224279319a689d7c63f9f2f396a
vini <thakurvinu00@gmail.com>
2024-08-02 19:10:34 +0530

    conversion

---
+++ conversionopearion.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+let val = false
+// console.log(typeof score)
+let stringtonum = Number(score)
+let boolcon = Number(val)
+console.log(typeof boolcon);
+console.log(boolcon);
+// console.log(typeof(stringtonum));
+// console.log(stringtonum);     //prints NaN
+
+/* 
+conversion opeartion
+"33" => 33
+"33abc' => NaN (Not a Number)
+true => 1
+
+*/
+
+
+let loggedin = 1
+let binarytobool = Boolean(loggedin)
+
+console.log(binarytobool);
+
+let somenum = 33
+let numtostring = String(somenum)
+console.log(typeof numtostring);
