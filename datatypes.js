e7f7309aaced37429c1835afb2a7f7f508270ac8
vini <thakurvinu00@gmail.com>
2024-08-02 18:35:58 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+"use strict" ;  //treat all js code as a newer  version
+
+// alert(5+3)  we are using node.js not browser this alert is not supported like this 
+
+let name = "vinay"  //string datatype
+let age = 2  //number datatype (have range of 2 to the power of 53)
+let isloggedin = true  // boolean datatype => true or false
+let state = null                  //  standalone value (empty)
+let isthere;                    //undefined (means variable is ok but no value is assigned)
+// symbol   =>      unique
+
+//object
+console.log(typeof isthere)
---
+++ variable.js
@@ -0,0 +1,8 @@
+const accnumber= 10000 // use to specify for fix vlaue
+let id = "vni@gmail.co"  // resolve the issue of  variable scope
+var haal = "kese ho"  // have issue of scope
+sim = "hello"
+
+// accnumber = 500;
+console.log(accnumber);
+console.table([id, haal, sim])
