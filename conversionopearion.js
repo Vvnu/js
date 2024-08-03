2cdb243be72eed799b304efefa9b159820a8da14
vini <thakurvinu00@gmail.com>
2024-08-03 17:17:53 +0530

    operation chapter completed

--- conversionopearion.js
+++ conversionopearion.js
@@ -32,5 +32,32 @@ let numtostring = String(somenum)
 
 let value = 3
 let negval = -value
-console.log(negval);
+// console.log(negval);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2**2);
 
+
+let str1 = "Vini"
+let str2 =  " bro"
+
+let str3 =  str1 + str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2" + 2)
+// console.log(1+ 2+ "2")
+// console.log(true);
+
+
+let num1, num2 ,num3
+
+num1 = num2 = num3 = 2+2
+console.log(num3);
+
+
+num3--;
+console.log(num3)
