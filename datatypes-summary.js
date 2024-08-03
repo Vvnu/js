8c4cd911c43c4f87c95f9b5290c274056ac11459
vini <thakurvinu00@gmail.com>
2024-08-03 19:00:38 +0530

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,48 @@
+/*  premitive datatype
+    :  they are call by value
+      original data is not given the copy of the data is given and changes applied on that copy.
+STRING,NUMBER,BOOLEAN,NULL,UNDEFINED, SYMBOL,BIGINT
+
+
+*/
+const score = 200;
+const scorevalue = 200.44
+const boolval = true
+const nulex = null
+let hello;
+const id = Symbol('123') // symbol datatype 
+const anotherid = Symbol('123')
+
+const  bignum = 5785765874687653445463563567567n   //bigint
+// console.log(bignum);
+// console.log ( id === anotherid)
+
+
+
+
+
+/*
+    non primitive datatype & Reference datatype: referenece can be allocated in memory for these datatypes
+
+ARRAY,OBJECTS,FUNCTIONS
+
+*/
+
+//Arrays in js
+
+const heros = ["Saktiman", "nagraj" , "Thor","FLying jatt"]
+const villian = ["killvish", "tatya bichu" , "doctor doom"]
+
+// console.log([heros]+villian);
+let obj  = 
+{
+    name:"vinay",   //object creation
+    age: 22,
+}
+// console.log(obj.name);
+
+
+const myfunction =  function vi(){          // function declaration and storing it in variable
+return "hey baby";
+}
+console.log(myfunction())
